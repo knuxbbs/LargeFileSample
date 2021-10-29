eb87a25f7aae5e32bdc3584b25e4a59eb9347d09
Bruno Bacelar <knuxbbs@gmail.com>
2021-10-28 23:25:06 -0300

    Remove the now ignored files

--- tests/LargeFilesSamples.Test/TestAppFactory.cs
+++
@@ -1,9 +0,0 @@
-using LargeFilesSample;
-using Microsoft.AspNetCore.Mvc.Testing;
-
-namespace LargeFilesSamples.Test
-{
-    public class TestAppFactory : WebApplicationFactory<Startup>
-    {
-    }
-}
